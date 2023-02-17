81186c1d93162a16b82a7b634db1d54013482b93
Austin <Martinaustin319@gmail.com>
2023-02-17 04:34:35 -0800

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
-#!/bin/bash
 # include <stdio.h>
-int main(){
-	char str[50];
-	str = "Programming is like building a multilingual puzzle";
-	puts(str);
-	return 0;
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
