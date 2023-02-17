455b262176b959ec3081c11c7b299477d9a77229
Austin <Martinaustin319@gmail.com>
2023-02-17 05:09:07 -0800

    printf fucntion

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 # include <stdio.h>
-/** main function that prints with proper grammar, but the outcome is a piece of art,
+/** main function that prints with proper grammar,
+ * 	 but the outcome is a piece of art,
  * return  zero always
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+	
 	return (0);
 }
