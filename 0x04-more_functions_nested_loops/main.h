336bb6ee8fce3ba8ad5304a5c6230a42d64e296a
Austin <Martinaustin319@gmail.com>
2023-02-23 12:16:41 -0800

    HEADER FILE

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,6 @@
-#ifndef HEADER_H
-#define HEADER_H
-# include <ctype.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-int check_uppercase(char c)
-{
-	if(isupper(c){
-		return (1);	
-	}
-	else{
-		return (0);
-	}
-}
+int _isupper(int c);
+
 #endif
