396f96da11e73cda2e86183a5965e0c3fb539899
Austin <Martinaustin319@gmail.com>
2023-02-24 11:58:32 -0800

    draw a line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -16,8 +16,12 @@ void print_line(int n)
 	{
 	_putchar('\n');
 	}
-	for (i = 0; i < n; ++i)
+	else
 	{
-	_putchar('_');
+		for (i = 0; i < n; ++i)
+		{
+		_putchar(95);
+		}
+		_putchar('\n');
 	}
 }
