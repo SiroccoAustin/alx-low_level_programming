3af816664a5d182bc3a5d902241784e473f0b09a
Austin <Martinaustin319@gmail.com>
2023-02-24 11:10:49 -0800

    print same numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 to 14 ten times
+ * Return: numbers from 10 to 14 printed 10 times
+ */
+
+void more_numbers(void)
+
+{
+
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 15)
+		{
+		_putchar(j + '0');
+		++j;
+		}
+		_putchar('\n');
+		++i;
+	}
+	_putchar('\n');
+}
