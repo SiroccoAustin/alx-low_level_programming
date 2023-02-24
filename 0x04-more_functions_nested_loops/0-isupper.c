19569c24cc77173b5304933344976839563302b7
Austin <Martinaustin319@gmail.com>
2023-02-24 04:39:23 -0800

    check uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,16 @@
 
 /**
  * _isupper - Checks for uppercase character.
- *
+ * @c: character to be checked
  * Return: 1 if character is uppercase, 0 otherwise.
  */
 
 int _isupper(int c)
 {
 
-	if (c >= 65 && c <= 90)
-	{
-	return (1);
-	}
-	return (0);
+	if (c >= 'A' && c <= 'Z')
+
+		return (1);
+	else	
+		return (0);
 }
