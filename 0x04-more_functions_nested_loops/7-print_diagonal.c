140ecae88a5f87b2b75ce0f829c5cd1781ad32de
Austin <Martinaustin319@gmail.com>
2023-02-24 13:22:09 -0800

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,9 +18,9 @@ void print_diagonal(int n)
 	}
 	else
 	{
-	for (i = 0; i < size; i++)
+	for (i = 0; i < n; i++)
     	{
-        for (j = 0; j < size; j++)
+        for (j = 0; j < i; j++)
         {
         _putchar(32);
         }
