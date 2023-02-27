1a11146b23678eb5800184b956544f53847af97b
Austin <Martinaustin319@gmail.com>
2023-02-27 11:00:36 -0800

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdout>
+
+/**
+ * _puts - prints out a string
+ * @str: parameter for a string
+ * Return: a string
+ */
+
+void _puts(char *str)
+{
+	_putchar(str);
+}
