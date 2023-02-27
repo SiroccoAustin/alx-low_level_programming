daadaacf726bd09f06bd2227782c516476cab0b4
Austin <Martinaustin319@gmail.com>
2023-02-27 04:45:40 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - output the length of the string
+ * @s: pointer variable of the string
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+
+{
+
+	strlen(*s);
+}
