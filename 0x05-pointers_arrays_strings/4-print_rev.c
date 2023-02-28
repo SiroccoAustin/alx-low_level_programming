d0d8fbd7654616a91180aa304ef4d1467d1831c5
Austin <Martinaustin319@gmail.com>
2023-02-28 01:21:14 -0800

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print a reversed string
+ * @s: pointer pointing to the string
+ * Return: a reversed string
+ */
+
+void print_rev(char *s)
+{
+
+	_putchar(strrev(s));
+}
