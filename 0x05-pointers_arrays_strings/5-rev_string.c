50772c32d8822119988da12ae04903fdf8a3c6ec
Austin <Martinaustin319@gmail.com>
2023-02-28 11:00:47 -0800

    reverse string function

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,5 +8,16 @@
 
 void rev_string(char *s)
 {
-	return (strrev(s));
+	int length = strlen(s);
+
+	int middle = length / 2;
+
+	char temp;
+
+	for (int i = 0; i < middle; i++)
+	{
+	temp = s[i];
+	s[i] = s[length - i - 1];
+	s[length - i - 1] = temp;
+	}
 }
