ebc14743bf136ca72e0bb7d256d735ad69d43d75
Austin <Martinaustin319@gmail.com>
2023-02-28 11:24:15 -0800

    print every character of the string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character of the string
+ * @str: pointer pointing to a string
+ * Returns: every character of the string
+ */
+
+void puts2(char *str)
+{
+	puts(str);
+}
