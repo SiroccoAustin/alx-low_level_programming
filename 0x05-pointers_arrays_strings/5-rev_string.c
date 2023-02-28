a08f995d2fa81a3d7b02ff71bec37ebea63ed84c
Austin <Martinaustin319@gmail.com>
2023-02-28 05:57:36 -0800

    return reversed string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * rev_string - returns a reversed string
+ * @s: pointer to a string
+ * Return: a reversed string
+ */
+
+void rev_string(char *s)
+{
+	return (strrev(s));
+}
