0a57d8d44b2ac4b063badacda4a5c4aa716911e0
Austin <Martinaustin319@gmail.com>
2023-03-01 05:20:36 -0800

    combine strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - returns a combination of two strings
+ * @dest: first argument
+ * @src: second argument
+ * Return: a combination of strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
