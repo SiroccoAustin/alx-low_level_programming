136e3e1213402bdc1064e7285aabae9d1f04a94c
Austin <Martinaustin319@gmail.com>
2023-03-01 21:46:17 -0800

    reversed array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,17 +10,14 @@
 
 void reverse_array(int *a, int n)
 {
-	int midpoint = n / 2;
 
 	int i = 0;
 
 	int temp[];
 
-	while (i < midpoint)
+	while (i < n)
 	{
 	temp = a[n - i - 1];
-	a[i] = a[n - i - 1];
-	a[n - i - 1] = temp;
 	i++;
 	}
 }
