21907adb69ddbd0ad9c81a68341cb68385381476
Austin <Martinaustin319@gmail.com>
2023-03-01 23:24:44 -0800

    convert to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,11 +10,14 @@
 
 char *string_toupper(char *temp)
 {
-	for (int i = 0; temp != '\0'; i++)
+	int i = 0; 
+
+	while (temp[i])
 	{
 	if (temp[i] >= 'a' && temp[i] <= 'z')
 	{
 	temp[i] = temp[i] - 32;
+	i++;
 	}
 	}
 }
