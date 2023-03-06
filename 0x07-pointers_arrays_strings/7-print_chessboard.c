ffc88ab2f7d8422d70337f05ed34635a55e11a06
Austin <Martinaustin319@gmail.com>
2023-03-06 11:25:20 -0800

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints out a chessboard
+ * @a: pointer to a multidimensional array
+ * Return: a chessboard
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j;
+
+	while (i < 8)
+	{
+	for (j = 0; j < 8; j++)
+	{
+	printf("%c",a[i][j]);
+	if (j == 7)
+	{
+	printf("\n");
+	}
+	}
+	i++;
+	}
+}
