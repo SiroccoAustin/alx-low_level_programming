b8bfef92db82af6c42edd77bee0d212fd4362f27
Austin <Martinaustin319@gmail.com>
2023-03-08 07:00:55 -0800

    puts in rec

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _puts_recursion - puts out a string
+ * @s: pointer to a string
+ * Return: a string
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
