f093ef17e7cc374f0a1456619ca90b26b098d4cf
Austin <Martinaustin319@gmail.com>
2023-03-14 09:42:16 -0700

    combine strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,19 +3,27 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * str_concat - outputs a combination of two strings
+ * @s1: first argument
+ * @s2: second argument
+ * Return: a combination of two strings
+ */
+
 char *str_concat(char *s1, char *s2)
 {
 	char *concat;
 
 	concat = (char *)malloc(sizeof(char));
 
-	if (concat != NULL)
-	{
-	concat = strcat(s1, s2);
-	}
-	else
+	if (concat == NULL)
 	{
 	return (NULL);
+	}
+	if (concat != NULL)
+	{
+	strcpy(concat, s1);
+	strcat(concat, s2);
 	}
 	return (concat);
 	
