63d17a5f68d1f01e4fe889b2ecc27031a5addcfd
Austin <Martinaustin319@gmail.com>
2023-03-14 11:03:52 -0700

    combine strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,13 +28,13 @@ char *str_concat(char *s1, char *s2)
 	strcpy(concat, s1);
 	strcat(concat, s2);
 	}
-	else if (s2 == NULL)
+	if (s2 == NULL)
 	{
 	s2 = " ";
 	strcpy(concat, s1);
 	strcat(concat, s2);
 	}
-	else if (s1 && s2 == NULL)
+	if (s1 && s2 == NULL)
 	{
 	s1 = " ";
 	s2 = " ";
