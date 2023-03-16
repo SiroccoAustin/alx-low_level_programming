8779780e6f547f218c47351182c0de50b23e26e0
Austin <Martinaustin319@gmail.com>
2023-03-16 03:36:55 -0700

    allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - outputs pointer address
+ * @b: argument
+ * Return: pointer address
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(int) * b);
+
+	if (ptr == NULL)
+	{
+	exit(98);
+	}
+	return (ptr);
+}
