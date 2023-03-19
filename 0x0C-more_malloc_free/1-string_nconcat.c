e0695b0444b1e770e144a21ce435253c6bc021c0
Austin <Martinaustin319@gmail.com>
2023-03-19 07:37:38 -0700

    Concatenate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,10 +19,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	ptr = malloc(sizeof(char) * length);
 
-	if (s1 == NULL)
+	/**if (s1 == NULL)
 	{
-	s1 = "";
-	}
+	s1 = ;
+	}*/
 
 	if (ptr != NULL)
 	{
