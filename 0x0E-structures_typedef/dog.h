3013446daae8ef12a6e759f38359bf27507633f5
Austin <Martinaustin319@gmail.com>
2023-03-20 12:40:11 -0700

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,17 @@
 #ifndef MACRO_H
 #define MACRO_H
 
+/**
+ *struct dog - a struct for dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 #endif /*MACRO_H*/
-				
