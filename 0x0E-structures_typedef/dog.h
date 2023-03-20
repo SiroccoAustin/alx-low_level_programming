f155ea8167ec34f8c715a60ce28c1132b08d1990
Austin <Martinaustin319@gmail.com>
2023-03-20 14:29:08 -0700

    HEADER FILE

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,18 +2,18 @@
 #define MACRO_H
 
 /**
- *struct dog - a struct for dog
+ *struct dog_t - new struct
  *@name: name of dog
  *@age: age of dog
  *@owner: owner of dog
  */
 
-struct dog
+typedef struct dog_t
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} my_dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /*MACRO_H*/
