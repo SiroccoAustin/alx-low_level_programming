6e076739c84f936b8a35da7c917770ac3d79a4ba
Austin <Martinaustin319@gmail.com>
2023-03-21 07:39:08 -0700

    copy string

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * new_dog - structure
+ * @name: argument
+ * @age: age argument
+ * @owner: owner argument
+ * Return: new structure
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *spot_dog;
@@ -13,16 +21,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	name_l = strlen(name);
 	owner_l = strlen(owner);
-	
+
 	spot_dog = malloc(sizeof(char) * (name_l + owner_l));
-	
+
 	if (spot_dog == NULL)
 	{
 	return (NULL);
 	}
 	spot_dog->name = malloc(sizeof(char) * name_l);
 	spot_dog->owner = malloc(sizeof(char) * owner_l);
-	
+
 	spot_dog->name = strcpy(spot_dog->name, name);
 	spot_dog->owner = strcpy(spot_dog->owner, owner);
 	spot_dog->age = age;
