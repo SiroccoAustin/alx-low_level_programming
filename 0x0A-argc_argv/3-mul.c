b4ed61aa28044125ce8823a266e30dbf5fddba38
Austin <Martinaustin319@gmail.com>
2023-03-25 00:40:13 -0700

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,12 +14,12 @@ int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 
-	if (argc == 1)
+	if (argc <= 2)
 	{
 	printf("Error\n");
 	return (1);
 	}
-	if (argc > 1)
+	if (argc >= 2)
 	{
 	for (i = 1; i < argc; i++)
 	{
