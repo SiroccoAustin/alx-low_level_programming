e0fa78d888273bdab6b2cf55bdfe0c32ff3b41a5
Austin <Martinaustin319@gmail.com>
2023-03-25 01:02:55 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,12 +15,12 @@ int main(int argc, char *argv[])
 {
 	int i, sum;
 
-	if (argc == 1)
+	if (argc <= 2)
 	{
 	printf("%d\n", 0);
 	return (0);
 	}
-	if (argc > 1)
+	if (argc > 2)
 	{
 	for (i = 1; i < argc; i++)
 	{
