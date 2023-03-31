8efb0e708a097b9995e53ccfbb1575a95cf6340e
Austin <Martinaustin319@gmail.com>
2023-03-31 01:55:13 -0700

    print before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void before_main(void) __attribute__((constructor));
+
+/**
+ * before_main - executed before main function
+ * Return: print string
+ */
+
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+};
+
