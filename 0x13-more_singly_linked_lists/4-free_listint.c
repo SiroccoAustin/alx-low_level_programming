54c5d5e8803a052fc2f28faa369c023442552b66
Austin <Martinaustin319@gmail.com>
2023-04-04 10:33:50 -0700

    free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_listint - free memory
+ * @head: pointer
+ * Return: free memory
+ */
+
 void free_listint(listint_t *head)
 {
 	listint_t *ptr;
