8dadaf5a6074028739592bb567d8a8eebcc85e9d
Austin <Martinaustin319@gmail.com>
2023-04-04 14:22:48 -0700

    delete node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - deletes first node
+ * @head: pointer
+ * Return: element
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	if (*head == NULL)
+	{
+	return (0);
+	}
+	
+	*head = (*head)->next;
+
+	return (temp->n);
+
+	free(temp);
+	
+	temp = NULL;
+}
