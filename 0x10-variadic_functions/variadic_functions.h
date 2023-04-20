14cad8d3ab26963572139bc54f26615cdc7bbd15
Austin <Martinaustin319@gmail.com>
2023-04-20 12:46:34 -0700

    HEADER FILE

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef VAR_H
+#define VAR_H
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /*VAR_H*/
