ca9d71cc0d4c37a34d924cc21ff1f68f76cea55c
Austin <Martinaustin319@gmail.com>
2023-04-21 02:51:46 -0700

    sum arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,15 +16,17 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 
 	va_start(ap, n);
-	
+
 	if (n == 0)
 	{
 	return (0);
 	}
-
+	else
+	{
 	for (i = 0; i < n; i++)
 	{
 	sum += va_arg(ap, int);
+	}
 	}
 	return (sum);
 }
