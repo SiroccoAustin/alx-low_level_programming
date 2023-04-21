30391dcc482cd26bb693cceadddb11f03d0d7a1c
Austin <Martinaustin319@gmail.com>
2023-04-21 12:12:37 -0700

    array iterate

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,6 +3,7 @@
 
 /**
  * array_iterator - iterate the array
+ * @array: array
  * @size: size of the array
  * @action: pointer function
  * Return: elements
@@ -11,6 +12,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
 	if (array && size && action)
 	{
 	for (i = 0; i < size; i++)
