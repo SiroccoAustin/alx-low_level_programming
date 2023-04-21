368ae75d06469e6d1e86679bc6d12c9c22e9d7e5
Austin <Martinaustin319@gmail.com>
2023-04-21 02:43:26 -0700

    HEADER FILE

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -13,6 +13,8 @@ typedef struct func
 	void (*f)(va_list list);
 } func_t;
 
+
+int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
