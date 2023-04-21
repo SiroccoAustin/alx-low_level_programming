6367b4332700c54fdd4adf007ecfecc3383c6739
Austin <Martinaustin319@gmail.com>
2023-04-21 14:16:20 -0700

    calculate

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of integers
+ * @a: first arg
+ * @b: second arg
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference between two int
+ * @a: first arg
+ * @b: second arg
+ * Return: diff
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul two int
+ * @a: first arg
+ * @b: second arg
+ * Return: mul
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div two int
+ * @a: first arg
+ * @b: second arg
+ * Return: div
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of two int
+ * @a: first arg
+ * @b: second arg
+ * Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
