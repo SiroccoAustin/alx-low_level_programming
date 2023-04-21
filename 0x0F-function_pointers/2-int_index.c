1e4df8555830ef14a7c1d8ab9108a91c4e2d7470
Austin <Martinaustin319@gmail.com>
2023-04-21 12:58:53 -0700

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - print index
+ * @array: array
+ * @size: size of the array
+ * @cmp: pointer to a function
+ * Return: index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size != 0)
+	{
+	for (i = 0; i < size; i++)
+	{
+	if (cmp(array[i]))
+	{
+	return(i);
+	}
+	}
+	}
+	return (-1);
+}
