6cf98df7220d5e549f0e7accbe0daa462a55410d
Austin <Martinaustin319@gmail.com>
2023-04-20 20:14:50 -0700

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,89 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+/**
+ * print_char - print characters
+ * @list: variadic list
+ * Return: character
+ */
+
+void print_char(va_list list)
+{
+	char c = va_arg(list, int);
+	printf("%c", c);
+}
+
+/**
+ * print_string - print string
+ * @list: variadic list
+ * Return: string
+ */
+
+void print_string(va_list list)
+{
+	char *s = va_arg(list, char *);
+	printf("%s", s);
+}
+
+/**
+ * print_int - print int
+ * @list: variadic list
+ * Return: int
+ */
+
+void print_int(va_list list)
+{
+	int i = va_arg(list, int);
+	printf("%d", i);
+}
+
+/**
+ * print_float - print float
+ * @list: variadic list
+ * Return: float
+ */
+
+void print_float(va_list list)
+{
+	double f = va_arg(list, double);
+	printf("%f", f);
+}
+
+/**
+ * print_all - print elements
+ * @format: string
+ * Return: elements
+ */
+
+void print_all(const char * const format, ...)
+{
+	func_t array[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	va_list list;
+
+	int i = 0, j;
+
+	va_start(list, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (array[j].s != NULL)
+		{
+			if (format[i] == *array[j].s)
+			{
+			array[i].f(list);
+			j++;
+			}
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+}
