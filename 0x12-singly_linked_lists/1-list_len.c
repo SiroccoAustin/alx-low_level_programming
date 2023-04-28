f92f701cc8c8e2879fef916860b3aad727fd9c84
Austin <Martinaustin319@gmail.com>
2023-04-28 12:31:09 -0700

    len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - count all the nodes
+ * @h: pointer to the node
+ * Return: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
