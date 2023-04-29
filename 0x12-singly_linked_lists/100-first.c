72a8a9147d5b13b6649644621255dd130cd129d4
Austin <Martinaustin319@gmail.com>
2023-04-29 12:22:06 -0700

    execute before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * BeforeMain - execute func
+ */
+void BeforeMain(void) __attribute__ ((constructor))
+
+void BeforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
