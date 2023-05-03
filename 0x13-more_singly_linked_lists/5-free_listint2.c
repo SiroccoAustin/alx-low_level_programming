7a199ae17a7d51273c5ca56669f113b5fa65027b
Austin <Martinaustin319@gmail.com>
2023-05-03 05:43:17 -0700

    free memory

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free memory
+ * @head: pointer to node
+ * Return: free memory and assign node to null
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while (*head != NULL)
+	{
+		ptr = *head;
+		ptr = ptr->next;
+		free(ptr);
+	}
+	*head = NULL;
+}
