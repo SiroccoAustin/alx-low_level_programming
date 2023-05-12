91086bcd3e678d63c9e28da89c918f10d8263116
Austin <Martinaustin319@gmail.com>
2023-05-12 11:18:18 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = creat(filename, O_RDWR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (text_content == NULL)
 	{
