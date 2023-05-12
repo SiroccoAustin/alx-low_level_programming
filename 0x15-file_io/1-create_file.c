41cc01c7253b9f0e1f6babc813e145753eca4998
Austin <Martinaustin319@gmail.com>
2023-05-12 02:53:15 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,18 @@ int create_file(const char *filename, char *text_content)
 
 	buffer = malloc(strlen(text_content));
 
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
 	fd = creat(filename, O_RDWR);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 
 	if (fd != -1)
 	{
