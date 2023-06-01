89945babe02502e83991013d6903ead8fbcc33bb
Austin <Martinaustin319@gmail.com>
2023-05-31 23:10:25 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - add numbers together
+ * @ac: first arg
+ * @av: second arg
+ * Return always returns 0
+ */
+
+int main(int ac, char *av[])
+{
+	int result = 0, i = 1;
+
+        if (ac == 1)
+        {
+                printf("%d\n", result);
+                return (1);
+        }
+        else
+        {
+                while (av[i])
+                {
+                        if (isalpha(*(av[i])))
+                        {
+                                printf("Error\n");
+                                return (1);
+                        }
+                        else
+                        {
+                                result += atoi(av[i]);
+                        }
+			i++;
+                }
+        }
+        printf("%d\n", result);
+	return (0);
+}
