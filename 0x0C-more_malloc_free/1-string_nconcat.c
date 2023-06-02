308b44c44051b8c4204a08cabb77453e0280aea6
Austin <Martinaustin319@gmail.com>
2023-06-02 05:23:24 -0700

    concatenate strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenate two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return: concatenated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i = 0;
+
+	unsigned int length = strlen(s2);
+
+	char *arr[2];
+
+	char *ptr;
+
+	ptr = malloc(sizeof(char *) * n);
+
+	arr[0] = s1;
+	arr[1] = s2;
+
+	while (i < 2)
+	{
+		if (arr[i] == NULL)
+		{
+			arr[i] = "";
+		}
+		i++;
+	}
+	if (n >= length)
+	{
+		strcpy(ptr, arr[1]);
+		return (ptr);
+	}
+	else
+	{
+		strcpy(ptr, arr[0]);
+		strncat(ptr, arr[1], n);
+	}
+	return (ptr);
+}
