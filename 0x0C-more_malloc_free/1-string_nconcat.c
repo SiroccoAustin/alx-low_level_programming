d524ac0c98a09c19f5d98c5e2bda0e02c6563660
Austin <Martinaustin319@gmail.com>
2023-06-03 04:14:37 -0700

    concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int i = 0;
 
-	unsigned int length, len1 = 0;
+	unsigned int length, len1 = 0, len2 = 0;
 
 	char *arr[2], *ptr;
 
@@ -38,7 +38,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= length && length != 0)
 	{
 		strcpy(ptr, arr[1]);
-		ptr[length] = '\0';
+		len2 = strlen(ptr);
+		ptr[len2] = '\0';
 		return (ptr);
 	}
 	strcpy(ptr, arr[0]);
