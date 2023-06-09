00571d0784c3b4f9c9208da0917a460813491354
Austin <Martinaustin319@gmail.com>
2023-06-09 14:13:49 -0700

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @ac: first arg
+ * @av: second arg
+ * Return: always return 0
+ */
+
+int main(int ac, char *av[])
+{
+	int mul = 1, i = 1;
+
+	if (ac == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	while (av[i])
+	{
+		mul *= atoi(av[i]);
+		i++;
+	}
+	printf("%d\n", mul);
+	return (0);
+
+}
