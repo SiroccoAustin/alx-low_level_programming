661ba1af4920705a1ca9c77cf4f7d7abf80434a9
Austin <Martinaustin319@gmail.com>
2023-06-12 22:56:43 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,7 +20,7 @@ char *_strdup(char *str)
 		length++;
 	}
 
-	ptr = malloc(sizeof(char *) * (length - sizeof(char *));
+	ptr = malloc(sizeof(char *) * (sizeof(char *) - length);
 
 	if (ptr == NULL)
 		return (NULL);
