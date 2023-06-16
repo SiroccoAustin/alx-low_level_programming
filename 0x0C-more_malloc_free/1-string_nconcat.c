115a8f077eff2df5e15656f1f03fe3bc666205fd
Austin <Martinaustin319@gmail.com>
2023-06-15 22:30:48 -0700

    concatenate strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenate strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return: new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0, len1 = 0, len2 = 0, w = 0, z = 0, j = 0;
+	char *ptr, *arr[2];
+
+	ptr = malloc(sizeof(char *) * (n + 1));
+	if (ptr == NULL)
+		return (NULL);
+	arr[0] = s1;
+	arr[1] = s2;
+	while (arr[i])
+	{
+		if (arr[i] == NULL)
+			arr[i] = "";
+		i++;
+	}
+	while (arr[0][len1])
+		len1++;
+	while (arr[1][len2])
+		len2++;
+	if (n >= len2 && len2 != 0)
+	{
+		while (arr[1][w])
+		{
+			ptr[w] = arr[1][w];
+			w++;
+		}
+		return (ptr);
+	}
+	while (arr[0][z])
+	{
+		ptr[z] = arr[0][z];
+		z++;
+	}
+	while (j <= n)
+	{
+		ptr[len1] = arr[1][j];
+		len1++;
+		j++;
+	}
+	return (ptr);
+}
