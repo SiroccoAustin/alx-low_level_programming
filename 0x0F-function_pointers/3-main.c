bce5b748d19b13284b1a5e819d9106417ad7278a
Austin <Martinaustin319@gmail.com>
2023-06-21 14:38:27 -0700

    calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,17 +1,39 @@
 #include "3-calc.h"
-#include <string.h>
+
+/**
+ * main - calculates basic math
+ * @ac: number of args
+ * @av: array of args
+ * Return: always return 0
+ */
 
 int main(int ac, char *av[])
 {
-	int f, num1, num2;
+	int num1, num2, result;
+
+	int (*f)(int, int);
 
 	if (ac < 4)
 	{
 		printf("Error\n");
-		exit(97);
+		exit(98);
+	}
+
+	if (!f)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (av[2] == '/' || av[2] == '%' && av[3] == 0)
+	{
+		printf("Error\n");
+		exit(100);
 	}
 	num1 = atoi(av[1]);
 	num2 = atoi(av[3]);
 	f = get_op_func(av[2]);
-	printf("%d\n", f(num1, num2));
+	result = f(num1, num2);
+	printf("%d\n", result);
+	return (0);
 }
