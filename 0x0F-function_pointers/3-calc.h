bc70e7f1737fa857d23f85a03468908759f5c4eb
Austin <Martinaustin319@gmail.com>
2023-06-23 03:15:29 -0700

    HEADER FILE

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,5 @@
 #ifndef CALC_H
 #define CALC_H
-
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,8 +12,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
