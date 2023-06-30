386d245ecfe300a9f8f754e2ce71ac8097b46898
Austin <Martinaustin319@gmail.com>
2023-06-30 10:33:36 -0700

    linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
+
 #include <stdio.h>
 
 /**
@@ -10,11 +11,12 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
