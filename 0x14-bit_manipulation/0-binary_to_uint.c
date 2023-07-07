706a5460c65b939b71ec23b61ce56bb1a5568b9b
Austin <Martinaustin319@gmail.com>
2023-07-07 05:21:51 -0700

    convert to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,16 +10,17 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0, result = 0;
 
+	if (!b)
+		return (0);
+
 	while (b[i])
 	{
 		result *= 2;
 
-		if (b[i] == '1')
-		{
-			result += 1;
-		}
+		if (isalpha(b[i]) != 0)
+			return (0);
 		else
-			result += 0;
+			result += b[i] == '1' ? 1 : 0;
 		i++;
 	}
 	return (result);
