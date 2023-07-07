5c6d6615cc9a206f4f5e7c9d02c3d8a083eeb4e7
Austin <Martinaustin319@gmail.com>
2023-07-07 13:32:08 -0700

    clear bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - set bit to 0
+ * @n: number
+ * @index: index
+ * Return: 1 if its successful and -1 if not
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask;
+
+	if (index > 63)
+		return (-1);
+	mask = 1 << pos;
+
+	*n = (*n & ~mask) | (0 << index);
+	return (1);
+}
