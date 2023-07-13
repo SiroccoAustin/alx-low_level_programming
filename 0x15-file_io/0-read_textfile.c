83c854a126aa6fe077368815a665817782cfdc23
Austin <Martinaustin319@gmail.com>
2023-07-13 11:04:03 -0700

    read and write

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	ssize_t readc, writec;
 
-	char *buf = malloc(letters);
+	char *buf = malloc(sizeof(char) * letters);
 
 	if (!filename)
 		return (0);
