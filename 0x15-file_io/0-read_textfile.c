91d308726307d79158fe741bf5aea921eebfb50b
Austin <Martinaustin319@gmail.com>
2023-07-13 10:57:18 -0700

    read and write

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads and writes contents of the file
+ * @filename: name of the file
+ * @letters: size of content to write
+ * Return: number of content written
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+
+	ssize_t readc, writec;
+
+	char buf[1024];
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	readc = read(fd, buf, letters);
+
+	if (readc == -1)
+		return (0);
+
+	writec = write(STDOUT_FILENO, buf, readc);
+
+	if (writec == -1)
+		return (0);
+
+	return (writec);
+}
