8c15af042985744cc973d0548233de32323454e7
Austin <Martinaustin319@gmail.com>
2023-07-14 06:26:59 -0700

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,6 +11,9 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, writec;
 
+	if (!filename)
+		return (0);
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
