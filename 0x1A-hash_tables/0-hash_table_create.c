141bfe488054ed7bb8d306ff6db11e712081e2a1
Austin <Martinaustin319@gmail.com>
2023-10-06 19:58:44 +0000

    hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,13 +9,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ptr;
 	unsigned long int index = 0;
-	if (size = 0)
+	if (size == 0)
 		return (NULL);
 
 	ptr = malloc(sizeof(hash_table_t));
 	if (ptr == NULL)
 		return (NULL);
-	ptr->array = malloc(sizeof(hash_node_t) * size);
+	ptr->array = malloc(sizeof(hash_node_t *) * size);
 	if (ptr->array == NULL)
 	{
 		free(ptr);
