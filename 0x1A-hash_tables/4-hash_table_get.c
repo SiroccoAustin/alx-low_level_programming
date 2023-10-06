f0a1a79dcb64af821b710dc2e5f521cb0dd3b9c7
Austin <Martinaustin319@gmail.com>
2023-10-06 20:30:08 +0000

    hash tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,10 +12,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 
 	char *value;
+	hash_node_t *ptr;
 
 	if (ht == NULL || ht->size == 0 || key == NULL || *key == 0)
 		return (NULL);
-	hash_node_t *ptr = ht->array[index];
+	ptr = ht->array[index];
 	while (ptr != NULL)
 	{
 		if (strcmp(ptr->key, key) == 0)
