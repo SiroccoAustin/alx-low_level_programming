866f47f763779cf791eb8dd194c67b1f7ed9e11e
Austin <Martinaustin319@gmail.com>
2024-01-17 12:37:28 -0800

    searching algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,20 @@
 #include "search_algos.h"
 
+/**
+ * linear_search - searches for a value
+ * @array: pointer to array
+ * @size: size of array
+ * @value: value to search for
+ * Return: index if sucessful or -1 if not
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i, index = 0;
 
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (index);
 		index++;
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,12 +1,26 @@
 #include "search_algos.h"
 
+/**
+ * binary_search - searches for a value
+ * @array: pointer to array
+ * @size: size of the array
+ * @value: value to search for
+ * Return: index if sucessful or -1 if not
+ */
+
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size - 1;
+	size_t left = 0, right = size - 1, i = 0;
+
+	printf("Searching in array");
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+	printf("\n");
 
 	while (left <= right)
 	{
 		int mid = left + (right - left) / 2;
+
 		if (array[mid] == value)
 			return (mid);
 		else if (array[mid] < value)
